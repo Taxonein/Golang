e80a6c6794fc2015d070b1a4da241445126dc2e3
Taxonein <taxonein@gmail.com>
2025-02-19 18:55:03 +0300

    211

--- ls2/map.go
+++ ls2/map.go
@@ -10,9 +10,18 @@ func main() {
 		"Dima": 3,
 	}
 
+	people1 := make(map[string]int)
+	people1["Kate"] = 128
+	delete(people1, "Kate")
+
+	fmt.Println(people1)
+
 	if val, ok := people["Tom"]; ok { //Если
 		fmt.Println(val)
 		fmt.Println(ok)
+	}
+	for key, value := range people {
+		fmt.Println(key, value)
 	}
 
 }
---
+++ ls2/pointer.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 6
+	fa := &a
+
+	var ff *int
+
+	fmt.Println(ff)
+
+	fmt.Println(fa)
+
+}
