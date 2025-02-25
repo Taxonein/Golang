15befa8f1cf95120506e156fd6cb543facb2ec8f
Taxonein <taxonein@gmail.com>
2025-02-25 16:48:59 +0300

    1

---
+++ ls2/interfaces3.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type file3 struct {
+	text string
+}
+type folder3 struct {
+	name string
+}
+
+type stream3 interface {
+	read() string
+	write(string)
+	close()
+}
+
+// *******
+
+func writeToStream(streamm stream3, text string) {
+	streamm.write(text)
+}
+func closeStream(streamm stream3) {
+	streamm.close()
+}
+
+// *******
+
+func (f file3) read() string {
+	return f.text
+}
+func (f file3) write(message string) {
+	f.text = message
+}
+func (f file3) close() {
+	fmt.Println("Файл закрыт: " + f.text)
+}
+
+//*******
+
+func main() {
+
+}
